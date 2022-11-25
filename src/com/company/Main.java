4d532c5de4693437dcecf1e4df15da017a3bf926
liuxianglin <liuxianglin2@outlook.com>
2022-11-25 14:44:20 +0800

    feat: 测试3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,24 +8,27 @@ import java.math.RoundingMode;
 import java.text.SimpleDateFormat;
 import java.time.Instant;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
 
     public static void main(String[] args) {
-        //价税合计 fen
-        Integer priceAndTaxCent = 300;
-        //税率
-        double taxRate = 0.03;
-        double taxCentD = priceAndTaxCent / (1 + taxRate) * taxRate;
-        Integer taxCent = new BigDecimal(taxCentD).setScale(1, RoundingMode.HALF_UP).intValue();
-        double a = 0.04;
-        double b = 0.03;
-        double c = a - b;
-        double diff = 1e-6f;
-        System.out.println(c);
-        System.out.println(c < diff);
-        System.out.println(c < a);
-        System.out.println(c < b);
+//        String msgBody = "  11  22  Å |Å|H₂O|10⁶|NaH₂PO₄|¥|•|・|￠|”|\"|/|㘵|䲞|m³?<>";
+        String msgBody = "91340100670942291C";
+        //	|
+        //|
+        //| |Å|H₂O|10⁶|NaH₂PO₄|¥|?|•|・|￠|”|"|/|㘵|䲞|m³
+//        Pattern p1 = Pattern.compile("\t|\r|\n|Å|H₂O|10⁶|NaH₂PO₄|¥|•|・|￠|”|\"|/|㘵|䲞|m³|[./?？|\\s]"); // 去除所有换行
+        Pattern p1 = Pattern.compile("^[A-Z0-9]{15}$|^[A-Z0-9]{17}$|^[A-Z0-9]{18}$|^[A-Z0-9]{20}$"); // 去除所有换行
+//        Pattern p2 = Pattern.compile("?"); // 去除所有换行
+        Matcher m1 = p1.matcher(msgBody);
+        msgBody = m1.replaceAll("");
+        System.out.println(msgBody);
+//        boolean matches = Pattern.matches("^[A-Z0-9]{15}$|^[A-Z0-9]{17}$|^[A-Z0-9]{18}$|^[A-Z0-9]{20}$", "000000000000000");
+        // 税号正则表达式
+        boolean matches = Pattern.matches("[^_IOZSVa-z\\W]{2}\\d{6}[^_IOZSVa-z\\W]{10}", "91340100670942291C");
+        System.out.println(matches);
     }
 
 
