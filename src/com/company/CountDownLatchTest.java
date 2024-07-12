75e4c85f19ff6f3b2742356058e55c655a333b6b
liuxianglin <liuxianglin2@outlook.com>
2024-07-12 11:12:25 +0800

    feat: 测试LocalDate 月份是否相等方法

--- src/com/company/CountDownLatchTest.java
+++ src/com/company/CountDownLatchTest.java
@@ -3,6 +3,8 @@ package com.company;
 import com.company.enums.BpmBusinessTypeEnum;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 /**
  * @author liuxianglin
@@ -13,6 +15,10 @@ public class CountDownLatchTest {
 
     public static void main(String[] args) {
         System.out.println(BpmBusinessTypeEnum.PAYMENT_BY_THE_OWNER.name());
+        LocalDate date1 = LocalDate.of(2024, 6, 17);
+        LocalDate date2 = LocalDate.of(2025, 7, 25);
+
+        System.out.println(date1.getMonth() ==  date2.getMonth());
     }
 
 
